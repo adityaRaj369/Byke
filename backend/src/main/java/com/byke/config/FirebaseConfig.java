843a9249c5847ca5dfd9519cbd5fab58e2c90c69
adityaRaj369 <adityaRaj369@gmail.com>
2026-03-12 23:58:17 +0530

    Deploy

--- backend/src/main/java/com/byke/config/FirebaseConfig.java
+++ backend/src/main/java/com/byke/config/FirebaseConfig.java
@@ -11,13 +11,15 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.FileInputStream;
+import java.io.FileInputStream;  
 import java.io.IOException;
+
+
 
 @Configuration
 @Slf4j
-public class FirebaseConfig {
-
+public class FirebaseConfig{ 
+     
     @Value("${firebase.credentials-path:}")
     private String firebaseCredentialsPath;
 

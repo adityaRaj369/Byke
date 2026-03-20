ac457c7e66304d2bd03d7bfe98a644121003a3b3
adityaRaj369 <aditya8789681673@gmail.com>
2026-03-20 21:01:46 +0530

    New Cde

--- mobile/rider-app/src/config/api.ts
+++ mobile/rider-app/src/config/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const api = axios.create({
-  baseURL: process.env.API_BASE_URL || 'http://13.60.42.120:8080/api',
+  baseURL: process.env.API_BASE_URL || 'http://16.171.230.164:8080/api',
   timeout: 30000,
   headers: {
     'Content-Type': 'application/json',

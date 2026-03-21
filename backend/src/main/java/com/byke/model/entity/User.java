e2bbc7a32fae75164b98e1d3ff8ebb9aded725cd
adityaRaj369 <aditya8789681673@gmail.com>
2026-03-21 15:33:49 +0530

    f

--- backend/src/main/java/com/byke/model/entity/User.java
+++ backend/src/main/java/com/byke/model/entity/User.java
@@ -1,17 +1,27 @@
 package com.byke.model.entity;
 
-import com.byke.model.enums.AccountStatus;
-import com.byke.model.enums.UserRole;
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
+import com.byke.model.enums.AccountStatus;
+import com.byke.model.enums.UserRole;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "users")
